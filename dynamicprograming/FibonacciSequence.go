450a741179935c47d156a6f4b164fe503059c37f
abel <abel210503@gmail.com>
2021-09-14 17:07:20 +0800

    [DP]FibonacciSequence

---
+++ dynamicprograming/FibonacciSequence.go
@@ -0,0 +1,29 @@
+package dynamicprograming
+
+// Base case: F(0) = F(1) = 1
+// Recursive case: F(n) = F(n-1) + F(n-2)
+
+var dp map[int]int
+
+func init() {
+	dp = make(map[int]int)
+}
+
+func fibonacciSequence(len int) int {
+	return recursiveCal(len)
+}
+
+func recursiveCal(n int) int {
+	if n <= 1 {
+		if _, ok := dp[n]; !ok {
+			dp[n] = 1
+		}
+		return dp[n]
+	}
+
+	if _, ok := dp[n]; !ok {
+		dp[n] = recursiveCal(n-1) + recursiveCal(n-2)
+	}
+
+	return dp[n]
+}
---
+++ dynamicprograming/FibonacciSequence_test.go
@@ -0,0 +1,37 @@
+package dynamicprograming
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_fibonacciSequence(t *testing.T) {
+	type args struct {
+		len int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{name: "case1", args: args{len: 0}, want: 1},
+		{name: "case2", args: args{len: 1}, want: 1},
+		{name: "case3", args: args{len: 2}, want: 2},
+		{name: "case4", args: args{len: 3}, want: 3},
+		{name: "case5", args: args{len: 4}, want: 5},
+		{name: "case6", args: args{len: 5}, want: 8},
+		{name: "case7", args: args{len: 6}, want: 13},
+		{name: "case8", args: args{len: 7}, want: 21},
+		{name: "case9", args: args{len: 8}, want: 34},
+		{name: "case10", args: args{len: 9}, want: 55},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := fibonacciSequence(tt.args.len); got != tt.want {
+				t.Errorf("fibonacciSequence() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+
+	fmt.Println(dp)
+}
